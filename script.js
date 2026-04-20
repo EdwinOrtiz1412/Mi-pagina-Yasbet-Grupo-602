93d597378a0c645eb118e7c110c6367fa81bcff3
EdwinOrtiz <edwin.oc31@gmail.com>
2026-04-20 14:43:04 -0600

    Revert "Correccion"
    This reverts commit f75920de04521172d8d5f3569359fc8b093ad1ba.

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 function mostrarMensaje (){
-    let mensaje = document.getElementById("mensaje1");
-    mensaje.innerHTML = "🐾 Cuida y protege a los animales";
-    mensaje.style.display = "block";
+    let mensaje=document.getElementById("mensaje1");
+    mensaje.innerHTML ="🐾Cuida y proteje a los animales";
+    mensaje.style.display = "black";
 
-    setTimeout(() => {
-        mensaje.style.opacity = "1";
-        mensaje.style.transform = "translateY(0)";
-    }, 50);
+ setTimeout (() => {
+    mensaje.style.opacity = "1";
+    mensaje.style.transform = "transformateY/(0)";
+  }, 50);
 }
+ 
 
 function mostrar_mensaje (){
-    let mensaje = document.getElementById("mensaje2");
-    mensaje.innerHTML = "🐾 Mensaje prueba";
-    mensaje.style.display = "block";
+    let mensaje=document.getElementById("mensaje2");
+    mensaje.innerHTML ="🐾Cuida,proteje a los animales";
+    mensaje.style.display = "black";
 
-    setTimeout(() => {
-        mensaje.style.opacity = "1";
-        mensaje.style.transform = "translateY(0)";
-    }, 50);
+ setTimeout (() => {
+    mensaje.style.opacity = "1";
+    mensaje.style.transform = "transformateY/(0)";
+ }, 50);
 }
+ 
